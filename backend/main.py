1b3e3112a11892f87d1e94d037d79d0b43661681
Klemen <flegar.klemen@gmail.com>
2025-10-27 13:21:53 +0100

    first commit

---
+++ backend/main.py
@@ -0,0 +1,195 @@
+# -------------------------------------------------------------------------------------------#
+#                                         Imports                                            #
+# -------------------------------------------------------------------------------------------#
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from yt_dlp import YoutubeDL
+from fastapi.responses import FileResponse
+import os
+import re
+import threading
+import time
+import hashlib
+import random
+from database import initializeDB, saveConversion, getLogs, getStats
+
+initializeDB()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+downloadDir = "downloads"
+if os.path.exists(downloadDir) == False:
+    os.mkdir(downloadDir, exist_ok=True)
+
+
+class request(BaseModel):
+    url: str
+    format: str
+    resolution: str | None = None
+
+
+# -------------------------------------------------------------------------------------------#
+#                                         API calls                                          #
+# -------------------------------------------------------------------------------------------#
+
+
+@app.post("/api/convert")
+async def convertVideo(payload: request):
+    print("Payload retrieved!", payload)
+
+    uniqueHash = generateHash()
+    ydl_opts = {}
+
+    if payload.format == "mp3":
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": os.path.join(downloadDir, f"%(title)s_{uniqueHash}.%(ext)s"),
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "postprocessor_cleanup": True,
+            "rm_cache_dir": True,
+            "no_cache_dir": True,
+            "cachedir": False,
+            "nopart": True,
+            "noplaylist": True,
+        }
+
+    elif payload.format == "mp4":
+        resolution = payload.resolution or "best"
+        ydl_opts = {
+            "format": f"bestvideo[height<={resolution}]+bestaudio/best",
+            "outtmpl": os.path.join(downloadDir, f"%(title)s_{uniqueHash}.%(ext)s"),
+            "merge_output_format": "mp4",
+            "postprocessor_cleanup": True,
+            "rm_cache_dir": True,
+            "no_cache_dir": True,
+            "cachedir": False,
+            "nopart": True,
+            "noplaylist": True,
+        }
+
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(payload.url, download=True)
+            filename = ydl.prepare_filename(info)
+            if filename is None:
+                raise ValueError("Filename could not be determined.")
+            if payload.format == "mp3":
+                filename = filename.rsplit(".", 1)[0] + ".mp3"
+            cleanName = os.path.basename(filename)
+            saveConversion(cleanName, payload.format)
+        return {
+            "status": "success",
+            "message": "Download complete",
+            "filename": cleanName,
+        }
+    except Exception as e:
+        print("Error while downloading: ", e)
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/api/download/{filename}")
+def downloadFile(filename: str):
+    filePath = os.path.join(downloadDir, filename)
+    if os.path.exists(filePath):
+        return FileResponse(
+            path=filePath, filename=filename, media_type="application/octet-stream"
+        )
+    else:
+        return {"status": "error", "message": "File not found"}
+
+
+@app.delete("/api/delete/{filename}")
+async def deleteFile(filename: str):
+    filePath = os.path.join(downloadDir, filename)
+    try:
+        if os.path.exists(filePath):
+            os.remove(filePath)
+            return {"status": "deleted"}
+        else:
+            return {"status": "not found"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/api/logs")
+def downloadLogs():
+    try:
+        logs = getLogs()
+        logsFilePath = "downloads/logs.txt"
+        with open(logsFilePath, "w", encoding="utf-8") as temp:
+            for row in logs:
+                temp.write(f"{row}\n")
+        return FileResponse(
+            path=logsFilePath, filename="logs.txt", media_type="text/plain"
+        )
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/api/stats")
+def downloadStats():
+    try:
+        stats = getStats()
+        return {
+            "status": "success",
+            "total_conversions": stats[1],
+            "number_of_mp3": stats[2],
+            "number_of_mp4": stats[3],
+        }
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+# -------------------------------------------------------------------------------------------#
+#                                    Garbage collector                                       #
+# -------------------------------------------------------------------------------------------#
+
+
+def deleteGarbage():
+    # on set interval delete all files / leftovers in downloads folder
+
+    while True:
+        startInt = time.time()
+        endInt = startInt - 30 * 60
+
+        filesList = os.listdir(downloadDir)
+
+        for file in filesList:
+            filePath = os.path.join(downloadDir, file)
+            if os.path.isfile(filePath):
+                if os.path.getatime(filePath) < endInt:
+                    try:
+                        os.remove(filePath)
+                        print(f"Deleted old file: {file}")
+                    except Exception as e:
+                        print(f"Error deleting {file}: {e}")
+
+        time.sleep(600)
+
+
+# start new thread
+threading.Thread(target=deleteGarbage, daemon=True).start()
+
+
+# -------------------------------------------------------------------------------------------#
+#                                     Helper functions                                       #
+# -------------------------------------------------------------------------------------------#
+
+
+def generateHash():
+    uniqueHash = hashlib.sha1(str(random.random()).encode()).hexdigest()[:8]
+    return uniqueHash
---
+++ frontend/script.js
@@ -0,0 +1,268 @@
+//-----------------------------------------------------------------------------------//
+//                                  DOM Elements                                     //
+//-----------------------------------------------------------------------------------//
+
+const logsLink = document.getElementById("logs_link");
+const statsLink = document.getElementById("stats_link");
+const toastContainer = document.getElementById("toast_container");
+const toastTitle = document.getElementById("toast_title");
+const toastBody = document.getElementById("toast_body");
+const xMark = document.getElementById("x_mark");
+const formatSelect = document.getElementById("convert_format");
+const resolutionSelect = document.getElementById("convert_resolution");
+const results = document.getElementById("convert_result");
+const submitButton = document.getElementById("submit_button");
+const downloadButton = document.getElementById("download_button");
+const ytURL = document.getElementById("yt_url");
+const invalidLinkFeedback = document.getElementById("invalid_link");
+const baseURL = `${window.location.protocol}//${window.location.hostname}:8000`;
+let downloadUrl = null;
+let filename = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+  formatSelect.value = "mp3";
+  resolutionSelect.hidden = true;
+  invalidLinkFeedback.hidden = true;
+  downloadButton.hidden = true;
+  submitButton.hidden = false;
+  submitButton.disabled = false;
+  downloadButton.disabled = false;
+  results.hidden = true;
+  logsLink.disabled = false;
+  toastContainer.classList.remove("slide-in");
+  toastContainer.classList.remove("fade-out");
+
+  if (ytURL.classList.contains("is-invalid")) {
+    ytURL.classList.remove("is-invalid");
+    invalidLinkFeedback.hidden = true;
+  }
+
+  formatSelect.addEventListener("change", () => {
+    resolutionSelect.hidden = formatSelect.value !== "mp4";
+  });
+
+  logsLink.addEventListener("click", downloadLogs);
+  statsLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    showStatsToast();
+  });
+
+  xMark.addEventListener("click", hideToast);
+
+  submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    submitRequest();
+  });
+});
+
+//-----------------------------------------------------------------------------------//
+//                                   Submit Request                                  //
+//-----------------------------------------------------------------------------------//
+
+async function submitRequest() {
+  if (downloadUrl) {
+    window.URL.revokeObjectURL(downloadUrl);
+    downloadUrl = null;
+    filename = null;
+  }
+
+  const url = ytURL.value.trim();
+  const format = formatSelect.value;
+
+  if (!url.includes("https://www.youtube.com/")) {
+    ytURL.classList.add("is-invalid");
+    invalidLinkFeedback.hidden = false;
+    return;
+  }
+
+  ytURL.classList.remove("is-invalid");
+  invalidLinkFeedback.hidden = true;
+  submitButton.disabled = true;
+  resolutionSelect.disabled = true;
+  formatSelect.disabled = true;
+
+  const payload = {
+    url: url,
+    format: format,
+    resolution: null,
+  };
+
+  if (format == "mp4") {
+    payload.resolution = resolutionSelect.value;
+  }
+
+  console.log(payload);
+  results.hidden = false;
+  results.textContent = "Converting...";
+
+  try {
+    const response = await fetch(`${baseURL}/api/convert`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (response.ok == false) {
+      results.textContent = "Error while sedning data!";
+      console.log("Error while sending data!");
+      return;
+    }
+
+    const data = await response.json();
+    if (data.status == "success") {
+      results.textContent = "Download is ready!";
+      downloadButton.hidden = false;
+      submitButton.hidden = true;
+
+      downloadButton.addEventListener("click", async () => {
+        downloadRequest(data);
+      });
+    } else {
+      results.textContent = "Error: " + data.message;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    results.textContent = "Error with backend connection!";
+  }
+}
+
+//-----------------------------------------------------------------------------------//
+//                                  Download Request                                 //
+//-----------------------------------------------------------------------------------//
+
+async function downloadRequest(data) {
+  const downloadLink = `${baseURL}/api/download/${encodeURIComponent(
+    data.filename
+  )}`;
+  downloadButton.disabled = true;
+  results.textContent = "Downloading...";
+
+  try {
+    const response = await fetch(downloadLink, {
+      cache: "no-store",
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    });
+
+    if (response.ok == false) {
+      results.textContent = "Error downloading file!";
+      downloadButton.disabled = false;
+      return;
+    }
+    const blob = await response.blob();
+    const objectURL = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    // create an invisible element to signalize when the download of the file is complete
+    a.href = objectURL;
+    a.download = data.filename;
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(objectURL);
+
+    results.textContent = "Download finished!";
+    submitButton.disabled = false;
+    submitButton.hidden = false;
+    downloadButton.disabled = false;
+    downloadButton.hidden = true;
+    resolutionSelect.disabled = false;
+    formatSelect.disabled = false;
+
+    // send signal to backend to delete the file
+    await fetch(`${baseURL}/api/delete/${encodeURIComponent(data.filename)}`, {
+      method: "DELETE",
+    });
+    window.location.reload();
+  } catch (error) {
+    console.error("Download error: ", error);
+    results.textContent = "Error while downloading the file!";
+    downloadButton.disabled = false;
+  }
+}
+
+//-----------------------------------------------------------------------------------//
+//                                    Download logs                                  //
+//-----------------------------------------------------------------------------------//
+
+async function downloadLogs() {
+  logsLink.disabled = true;
+  const resposne = await fetch(`${baseURL}/api/logs`);
+  const blob = await resposne.blob();
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+
+  a.href = url;
+  a.download = "logs.txt";
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+
+  await fetch(`${baseURL}/api/delete/logs.txt`, {
+    method: "DELETE",
+  });
+  logsLink.disabled = false;
+}
+
+//-----------------------------------------------------------------------------------//
+//                                      Toast                                        //
+//-----------------------------------------------------------------------------------//
+
+async function showStatsToast() {
+  try {
+    const response = await fetch(`${baseURL}/api/stats`);
+    if (response.ok == false) {
+      throw new Error("Error while fetching server stats!");
+    }
+    const stats = await response.json();
+    toastTitle.textContent = "Page stats";
+    toastBody.innerHTML = `
+      Total conversions: ${stats.total_conversions}
+      &nbsp;&nbsp;&nbsp;&nbsp;MP3: ${stats.number_of_mp3}
+      &nbsp;&nbsp;&nbsp;&nbsp;MP4: ${stats.number_of_mp4}`;
+    showToast();
+    setTimeout(() => {
+      hideToast();
+    }, 5000);
+  } catch (error) {
+    toastTitle.textContent = "Error";
+    toastBody.textContent = "Could not load stats!";
+    console.error(error);
+    showToast();
+    setTimeout(() => {
+      hideToast();
+    }, 5000);
+  }
+}
+
+function showToast() {
+  toastContainer.hidden = false;
+  toastContainer.classList.remove("fade-out");
+  toastContainer.classList.add("slide-in");
+}
+
+function hideToast() {
+  toastContainer.classList.remove("slide-in");
+  toastContainer.classList.remove("fade-out");
+  toastContainer.classList.add("fade-out");
+
+  toastContainer.addEventListener(
+    "animationend",
+    () => {
+      toastContainer.hidden = true;
+      toastContainer.classList.remove("fade-out");
+    },
+    { once: true }
+  );
+}
