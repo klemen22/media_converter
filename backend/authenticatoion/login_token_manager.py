e229fd118f1b573ad09da242beb7e9bea3d5ea2f
Klemen <flegar.klemen@gmail.com>
2025-12-14 14:02:49 +0100

    /

--- backend/authenticatoion/login_token_manager.py
+++ backend/authenticatoion/login_token_manager.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from fastapi import Depends
 from fastapi.responses import JSONResponse
+from fastapi import HTTPException
+from starlette import status
 from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordBearer
 from database import getUsers
@@ -38,16 +40,34 @@ def createToken(data: dict):
 # -------------------------------------------------------------------------------------------#
 
 
+# can't use JSONresponse here otherwise this happens:
+
+"""
+{
+  "status": "success",
+  "user": {
+    "status": "error",
+    "message": "<class 'jose.exceptions.JWTError'>"
+  }
+}
+"""
+
+
 def checkTokenUser(token: str = Depends(oauth2Scheme)):
     try:
         payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
         username = payload.get("sub")
         if username is None:
-            return {"status": "error", "message": "Could not valiedate the user."}
+            return HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate user",
+            )
         return username
 
     except JWTError:
-        return {"status": "error", "message": str(JWTError)}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired or invalid"
+        )
 
 
 # -------------------------------------------------------------------------------------------#
